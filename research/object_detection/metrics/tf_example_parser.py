ff420f9a19bf81f9ac5e494056ea14dbf5a92951
Federico Lopez <flopez@genium.io>
2018-11-14 17:49:34 -0300

    Fixed as per issue #3252

--- research/object_detection/metrics/tf_example_parser.py
+++ research/object_detection/metrics/tf_example_parser.py
@@ -44,9 +44,13 @@ class StringParser(data_parser.DataToNumpyParser):
     self.field_name = field_name
 
   def parse(self, tf_example):
-    return "".join(tf_example.features.feature[self.field_name]
-                   .bytes_list.value) if tf_example.features.feature[
-                       self.field_name].HasField("bytes_list") else None
+    """Fixed as per issue #3252"""
+    if tf_example.features.feature[self.field_name].HasField("bytes_list"):
+        result = tf_example.features.feature[self.field_name].bytes_list.value
+        result = "".join([x if type(x)=='str' else x.decode('utf-8', 'ignore') for x in result])
+    else:
+        result = None
+    return result
 
 
 class Int64Parser(data_parser.DataToNumpyParser):
